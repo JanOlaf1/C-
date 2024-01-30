cd4c301441f72c57c783157fbc3ade2991c9b9a1
JanOlaf1 <122294366+JanOlaf1@users.noreply.github.com>
2024-01-30 10:49:36 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perintovero.information;
+using Perintovero.distant_family;
+using Perintovero.close_family;
 
-namespace Pinta_ala_laskuri
+namespace Perintovero
 {
     internal class Program
     {
@@ -8,82 +15,39 @@ namespace Pinta_ala_laskuri
         {
             while (true)
             {
-                /*Annetaan ohjelman vaihtoehdot*/
-                Console.WriteLine("Valitse toiminto:");
-                Console.WriteLine("1. Ympyrän pinta-ala");
-                Console.WriteLine("2. Nelikulmion pinta-ala");
-                Console.WriteLine("3. Kolmion pinta-ala");
-                Console.WriteLine("L. Lopetus");
-
+                /*2 eri luokkaa mm. lähimmät sukulaiset ja muut kuin lähimmät sukulaiset*/
+                Console.WriteLine("Valitse veroluokkasi");
+                Console.WriteLine("1. Lähimmäiset sukulaiset");
+                Console.WriteLine("2. Muut kuin lähimmät sukulaiset");
+                Console.WriteLine("3. Lisätietoaa vaihtoehdoista.");
+                Console.WriteLine("L. Lopeta ohjelma");
                 string valinta = Console.ReadLine();
-
                 switch (valinta)
                 {
-                    /*Lasketaan ympyrän pinta-ala*/
                     case "1":
-                        LaskeYmpyranPintaAla();
+                        Perintovero.close_family.Perintovero.LaskePerintoVero();
                         break;
-                    /*Lasketaan nelikulmion pinta-ala*/
-
                     case "2":
-                        LaskeNelikulmionPintaAla();
+                        Perintovero.distant_family.DistantFamily.DistantFamilyToiminnallisuus();
                         break;
-                    /*Lasketaan kolmio pinta-ala*/
-
                     case "3":
-                        LaskeKolmionPintaAla();
+                        Information information = new Information();
+                        information.InformationToiminnallisuus();
                         break;
-                    /*Mahdollisuus  lopettamiseen*/
-
                     case "L":
                     case "l":
                         Console.WriteLine("Ohjelma sulkeutuu.");
                         return;
-                        /*Lisätään ehto jos valitaan väärin.*/
+                    /*Väärä valinta*/
                     default:
-                        Console.WriteLine("Virheellinen valinta. Yritä uudelleen.");
+                        Console.WriteLine("Virheellinen valinta. Yritä uudelleen (L. Lopettaa ohjelman");
                         break;
+                        
                 }
-
                 Console.WriteLine();
+
+
             }
-        }
-        /*Lasketaan ympyrän pinta-ala*/
-
-        static void LaskeYmpyranPintaAla()
-        {
-            Console.Write("Anna ympyrän säde: ");
-            double sade = double.Parse(Console.ReadLine());
-
-            double pintaAla = Math.PI * Math.Pow(sade, 2);
-            Console.WriteLine($"Ympyrän pinta-ala on {pintaAla:F2}");
-        }
-        /*Lasketaan nelikulmion pinta-ala*/
-
-        static void LaskeNelikulmionPintaAla()
-        {
-            Console.Write("Anna nelikulmion leveys: ");
-            double leveys = double.Parse(Console.ReadLine());
-
-            Console.Write("Anna nelikulmion korkeus: ");
-            double korkeus = double.Parse(Console.ReadLine());
-
-            double pintaAla = leveys * korkeus;
-            Console.WriteLine($"Nelikulmion pinta-ala on {pintaAla:F2}");
-        }
-        /*Lasketaan kolmio pinta-ala*/
-
-        static void LaskeKolmionPintaAla()
-        {
-            Console.Write("Anna kolmion kanta: ");
-            double kanta = double.Parse(Console.ReadLine());
-
-            Console.Write("Anna kolmion korkeus: ");
-            double korkeus = double.Parse(Console.ReadLine());
-
-            double pintaAla = 0.5 * kanta * korkeus;
-        /*Formatoidaan eri tavalla vaihtelun vuoksi.*/
-            Console.WriteLine("Kolmion pinta-ala on " + pintaAla.ToString("F2"));
         }
     }
 }
